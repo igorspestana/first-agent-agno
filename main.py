f5a75532b603382a2d7a30c274a41f033cc022ec
igorspestana <igorspestana@gmail.com>
2025-08-13 21:41:21 -0300

    Refatora a configuração do agente para utilizar SqliteStorage, remove ferramentas desnecessárias e atualiza a chamada de print_response para novos testes. Adiciona sqlalchemy como dependência no pyproject.toml e uv.lock.

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import os
 from agno.agent import Agent
 from agno.models.google import Gemini
 from dotenv import load_dotenv
-from agno.tools.googlesearch import GoogleSearchTools
-from agno.tools.reasoning import ReasoningTools
+from agno.storage.sqlite import SqliteStorage
+
+storage = SqliteStorage(table_name="agno_sessions", db_file="tmp/agents.db")
 
 # Carrega as variáveis de ambiente
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
@@ -12,24 +13,17 @@ load_dotenv(os.path.join(BASEDIR, '.env'))
 
 # Configura o agente com a chave de API
 agent = Agent(
+    session_id="s_124",
+    id="u_1",
     model=Gemini(
         id=os.environ['DEFAULT_MODEL'],
         api_key=os.environ['GOOGLE_API_KEYS']
     ),
-    instructions=[
-        "Use tabelas para organizar as informações.",
-        "Inclua apenas a tabela na resposta. Sem nenhum outro texto."
-    ],
-    tools=[
-        ReasoningTools(add_instructions=True),
-        GoogleSearchTools()
-    ],
+    add_history_to_messages=True,
+    storage=storage,
 )
 
-agent.print_response(
-    "Faça um comparativo entre os países da Europa e os países da América Latina, com base na densidade populacional.", 
-    stream=True,
-    show_tool_calls=True,
-    show_function_calls=True,
-    stream_intermediate_steps=True,
-)
+if __name__ == "__main__":
+    # agent.print_response("Meu nome é Pedro, e eu tenho 26 anos.")
+    agent.print_response("Qual é o meu nome?")
+    agent.print_response("Qual é a minha idade?")
